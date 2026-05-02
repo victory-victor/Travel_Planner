dfd3876da631c6325635b049d040e375cbc810dc
Harsh Jha <victory25050@gmail.com>
2026-05-02 14:25:34 +0530

    fixed email issue

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,9 +1,12 @@
 const nodemailer = require('nodemailer');
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: false,
+  port: Number(process.env.EMAIL_PORT),
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
